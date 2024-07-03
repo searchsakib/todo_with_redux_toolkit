662ba0ce62b7104030b29c78b062ec5c8363d056
searchsakib <searchsakib@gmail.com>
2024-07-04 02:17:01 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h2>hello man!</h2>
+      <Button variant='destructive'>This</Button>
+    </>
+  );
+}
+
+export default App;
