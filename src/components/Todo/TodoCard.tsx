a8e94768088abeacd01d8007f9530df91dec5b11
searchsakib <searchsakib@gmail.com>
2024-07-04 05:19:39 +0600

    gradient border added

--- src/components/Todo/TodoCard.tsx
+++ src/components/Todo/TodoCard.tsx
@@ -1,6 +1,6 @@
 const TodoCard = () => {
   return (
-    <div className='flex items-center justify-evenly border p-3 font-medium rounded-lg text-[#ffcf20]'>
+    <div className='flex items-center justify-evenly border border-slate-400 p-3 font-medium rounded-lg'>
       <input type='checkbox' name='checkbox' id='checkbox' />
       <p>Todo Title</p>
       <p>Time</p>
--- src/components/Todo/TodoContainer.tsx
+++ src/components/Todo/TodoContainer.tsx
@@ -2,13 +2,18 @@ import TodoCard from './TodoCard';
 
 const TodoContainer = () => {
   return (
-    <div className='bg-[#282C34] p-10'>
+    <div className='p-10'>
       <div>
         <button>Add Todo</button>
         <button>Filter</button>
       </div>
-      <div className='space-y-4'>
-        <TodoCard></TodoCard>
+      <div className='bg-primary-gradient w-full h-full rounded-md p-[5px]'>
+        <div className='space-y-3 bg-white p-5'>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+        </div>
       </div>
       {/* <div className='text-[#ffcf20] text-center text-2xl'>No Todos Here!</div> */}
     </div>
